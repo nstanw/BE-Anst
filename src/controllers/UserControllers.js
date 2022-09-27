fbd50505538409cb08d7bc382f97078e63fcb391
T.Anh <langthambca@gmail.com>
2022-09-27 13:55:08 +0700

    code do

--- src/controllers/UserControllers.js
+++ src/controllers/UserControllers.js
@@ -1,22 +1,71 @@
-const User = require('../models/UserModels')
-const bcrypt = require('bcrypt')
+const User = require('../models/UserModels');
+const bcrypt = require('bcrypt');
 
-exports.register = function(req, res, next){    
-    User.findOne({email: req.body.email}, (err, user) => {
-        if(user == null){ //Kiểm tra xem email đã được sử dụng chưa
-            bcrypt.hash(req.body.password, 10, function(err, hash){ //Mã hóa mật khẩu trước khi lưu vào db
-                if (err) {return next(err);}
-                const user = new User(req.body)
-                user.role = ['customer'] //sau khi register thì role auto là customer
-                user.password = hash;
-                user.password_confirm = hash;
-                user.save((err, result) => {
-                    if(err) {return res.json({err})}
-                    res.json({user: result})
-                })
-            })
-        }else{
-            res.json({err: 'Email has been used'})
-        }
-    })
-}
+exports.changeImg = function (req, res, next) {
+  if (err) {
+    return next(err);
+  }
+  const user = new User(req.body);
+  user.save((err, result) => {
+    if (err) {
+      return res.json({ err });
+    }
+    res.json({ user: result });
+  });
+};
+
+exports.register = function (req, res, next) {
+  const user = new User(req.body);
+
+  user.save((err, result) => {
+    if (err) {
+      return res.json({ err });
+    }
+    res.json({ result });
+  });
+};
+exports.updateUser = function (req, res, next) {
+  const filter = {
+    _id: '633267ace2371d4648af00aa',
+  };
+  const update = req.body
+ const  t =  User.findOneAndUpdate(filter, update,{upsert: false}, (err, result) => {
+  if (err) return res.send(500, {error: err});
+  return res.send('Succesfully saved.');
+ })
+};
+exports.getUser = function (req, res, next) {
+  const filter = {
+    _id: '633267ace2371d4648af00aa',
+  };
+  User.findOne(filter, (err, result) => {
+  if (err) return res.send(500, {error: err});
+  return res.send(result);
+ })
+};
+
+// exports.register = function (req, res, next) {
+//   User.findOne({ email: req.body.email }, (err, user) => {
+//     if (user == null) {
+//       //Kiểm tra xem email đã được sử dụng chưa
+//       bcrypt.hash(req.body.password, 10, function (err, hash) {
+//         //Mã hóa mật khẩu trước khi lưu vào db
+//         if (err) {
+//           return next(err);
+//         }
+//         const user = new User(req.body);
+//         user.role = ['customer']; //sau khi register thì role auto là customer
+//         user.password = hash;
+//         user.password_confirm = hash;
+//         user.save((err, result) => {
+//           if (err) {
+//             return res.json({ err });
+//           }
+//           res.json({ user: result });
+//         });
+//       });
+//     } else {
+//       res.json({ err: 'Email has been used' });
+//     }
+//   });
+// };
--- src/models/UserModels.js
+++ src/models/UserModels.js
@@ -5,8 +5,12 @@ const userSchema = new Schema({
     email: {type: String, unique: true, required: true, trim: true},
     username: {type: String, required: true, trim: true, minlength: 2},
     role: {type: String, enum: ['admin', 'customer']},
-    password: {type: String, required: true, trim: true, minlength: 6},
-    password_confirm: {type: String, required: true, trim: true, minlength: 6},
-});
+    password: {type: String,  trim: true, minlength: 6},
+    password_confirm: {type: String, trim: true, minlength: 6},
+    avatar: {type: String,  trim: true, minlength: 2},
+    image: {type: String, trim: true, minlength: 2},
+    video: {type: String, trim: true, minlength: 2},
+    
+},{timestamp : true});
 
 module.exports = mongoose.model('User', userSchema)
